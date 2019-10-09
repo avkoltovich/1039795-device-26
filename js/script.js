48febddf067e310b1ef53a660f846e7754c6cd65
Aleksandr Koltovich <avkoltovich@gmail.com>
2019-10-09 22:06:48 +0300

    Переписал слайдер на цикл

--- js/script.js
+++ js/script.js
@@ -1,104 +1,57 @@
-// Слайдер
+// Слайдер на цикле
 
-var slideButtonOne = document.querySelector('.slide__btn--one');
-var slideButtonTwo = document.querySelector('.slide__btn--two');
-var slideButtonThree = document.querySelector('.slide__btn--three');
+var slideButtons = document.querySelectorAll('.slider__control');
+var slides = document.querySelectorAll('.slider__item');
 
-var slideOne = document.querySelector('.slide__one');
-var slideTwo = document.querySelector('.slide__two');
-var slideThree = document.querySelector('.slide__three');
+var removeClass = function () {
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].classList.remove('slider__show');
+  };
+  for (var i = 0; i < slideButtons.length; i++) {
+    slideButtons[i].classList.remove('current-slide');
+  };
+};
 
-slideButtonOne.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  slideOne.classList.remove('slider__show');
-  slideTwo.classList.remove('slider__show');
-  slideThree.classList.remove('slider__show');
-  slideOne.classList.add('slider__show');
+var addButtonClickHandler = function (slide, btn) {
+  btn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    removeClass();
+    slide.classList.add('slider__show');
+    btn.classList.add('current-slide');
+  });
+};
 
-  slideButtonOne.classList.remove('current-slide');
-  slideButtonTwo.classList.remove('current-slide');
-  slideButtonThree.classList.remove('current-slide');
-  slideButtonOne.classList.add('current-slide');
-});
+for (var i = 0; i < slideButtons.length; i++) {
+  addButtonClickHandler(slides[i], slideButtons[i]);
+}
 
-slideButtonTwo.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  slideOne.classList.remove('slider__show');
-  slideTwo.classList.remove('slider__show');
-  slideThree.classList.remove('slider__show');
-  slideTwo.classList.add('slider__show');
+// Блок Сервис на цикле
 
-  slideButtonOne.classList.remove('current-slide');
-  slideButtonTwo.classList.remove('current-slide');
-  slideButtonThree.classList.remove('current-slide');
-  slideButtonTwo.classList.add('current-slide');
-});
+var serviceItems = document.querySelectorAll('.service__item');
+var serviceButtons = document.querySelectorAll('.service__button');
+var serviceSlides = document.querySelectorAll('.service__text-wrapper');
 
-slideButtonThree.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  slideOne.classList.remove('slider__show');
-  slideTwo.classList.remove('slider__show');
-  slideThree.classList.remove('slider__show');
-  slideThree.classList.add('slider__show');
+var removeClass = function () {
+  for (var i = 0; i < serviceSlides.length; i++) {
+    serviceSlides[i].classList.remove('service__show');
+  };
+  for (var i = 0; i < serviceItems.length; i++) {
+    serviceItems[i].classList.remove('service__item--current');
+  };
+};
 
-  slideButtonOne.classList.remove('current-slide');
-  slideButtonTwo.classList.remove('current-slide');
-  slideButtonThree.classList.remove('current-slide');
-  slideButtonThree.classList.add('current-slide');
-});
+var addButtonClickHandler = function (slide, item, btn) {
+  btn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    removeClass();
+    slide.classList.add('service__show');
+    item.classList.add('service__item--current');
+  });
+};
 
-// Блок Сервис
-
-var serviceItemDeliver = document.querySelector('.service__item--deliver');
-var serviceItemWaranty = document.querySelector('.service__item--waranty');
-var serviceItemCredit = document.querySelector('.service__item--credit');
-
-var serviceButtonDeliver = serviceItemDeliver.querySelector('.service__button--deliver');
-var serviceButtonWaranty = serviceItemWaranty.querySelector('.service__button--waranty');
-var serviceButtonCredit = serviceItemCredit.querySelector('.service__button--credit');
-
-var serviceWrapperDeliver = document.querySelector('.service__text-wrapper--deliver');
-var serviceWrapperWaranty = document.querySelector('.service__text-wrapper--waranty');
-var serviceWrapperCredit = document.querySelector('.service__text-wrapper--credit');
-
-serviceButtonDeliver.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  serviceWrapperDeliver.classList.remove('service__show');
-  serviceWrapperWaranty.classList.remove('service__show');
-  serviceWrapperCredit.classList.remove('service__show');
-  serviceWrapperDeliver.classList.add('service__show');
-
-  serviceItemDeliver.classList.remove('service__item--current');
-  serviceItemWaranty.classList.remove('service__item--current');
-  serviceItemCredit.classList.remove('service__item--current');
-  serviceItemDeliver.classList.add('service__item--current');
-});
-
-serviceButtonWaranty.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  serviceWrapperDeliver.classList.remove('service__show');
-  serviceWrapperWaranty.classList.remove('service__show');
-  serviceWrapperCredit.classList.remove('service__show');
-  serviceWrapperWaranty.classList.add('service__show');
-
-  serviceItemDeliver.classList.remove('service__item--current');
-  serviceItemWaranty.classList.remove('service__item--current');
-  serviceItemCredit.classList.remove('service__item--current');
-  serviceItemWaranty.classList.add('service__item--current');
-});
-
-serviceButtonCredit.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  serviceWrapperDeliver.classList.remove('service__show');
-  serviceWrapperWaranty.classList.remove('service__show');
-  serviceWrapperCredit.classList.remove('service__show');
-  serviceWrapperCredit.classList.add('service__show');
-
-  serviceItemDeliver.classList.remove('service__item--current');
-  serviceItemWaranty.classList.remove('service__item--current');
-  serviceItemCredit.classList.remove('service__item--current');
-  serviceItemCredit.classList.add('service__item--current');
-});
+for (var i = 0; i < serviceButtons.length; i++) {
+  addButtonClickHandler(serviceSlides[i], serviceItems[i], serviceButtons[i]);
+}
 
 // Карта
 
@@ -121,27 +74,27 @@ closeMapBtn.addEventListener("click", function (evt) {
 
 ymaps.ready(function () {
   var myMap = new ymaps.Map('map', {
-          center: [55.686980, 37.529654],
-          zoom: 16
-      }, {
-          searchControlProvider: 'yandex#search'
-      }),
+      center: [55.686980, 37.529654],
+      zoom: 16
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
 
-      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-      ),
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
 
-      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-          hintContent: 'DEVICE',
-      }, {
-          iconLayout: 'default#image',
-          iconImageHref: 'img/popular-2.svg',
-          iconImageSize: [40, 70],
-          iconImageOffset: [-20, -70]
-      });
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      hintContent: 'DEVICE',
+    }, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/popular-2.svg',
+      iconImageSize: [40, 70],
+      iconImageOffset: [-20, -70]
+    });
 
   myMap.geoObjects
-      .add(myPlacemark);
+    .add(myPlacemark);
 });
 
 // Модальное окно
